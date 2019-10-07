987447e0c0fb868aea225dd169e53168de7cb0e2
Brayon <braydonalan@gmail.com>
2019-10-06 23:32:16 -0400

    UI Changes
    UI changes when editing and viewing an event.

--- resources/views/event/edit.blade.php
+++ resources/views/event/edit.blade.php
@@ -5,123 +5,119 @@
 
 	<div class="container">
 		<div class="flex w-full justify-center">
-{{--			<div class="row">--}}
-{{--				<div class="col">--}}
-					<div class="card shadow w-full sm:w-3/4 lg:w-1/2">
-						<div class="card-body">
-							<div class="flex self-end justify-between border-grey-dark border-b-2 mb-2">
-								<div class="self-center text-sm md:text-lg lg:text-2xl ">
-									<i class="fas fa-calendar-plus"></i> {{ $event->name }}
-								</div>
-								<form action="{{ route('event.destroy', ['id' => $event->id]) }}" method="POST">
-									@csrf
-									@method('DELETE')
-									<button class="btn text-sm md:text-base hover:bg-red-lightest text-red-darkest lg:text-lg">
-										<i class="fas fa-dumpster-fire"></i> Delete
-									</button>
-								</form>
-							</div>
-							<form action="{{ route('event.update', $event ) }}" method="POST">
-								@csrf
-								@method('PUT')
+			<div class="card shadow w-full sm:w-3/4 lg:w-1/2">
+				<div class="card-body">
+					<div class="flex self-end justify-between border-grey-dark border-b-2 mb-2">
+						<div class="self-center text-sm md:text-lg lg:text-2xl ">
+							<i class="fas fa-calendar-plus"></i> {{ $event->name }}
+						</div>
+						<form action="{{ route('event.destroy', ['id' => $event->id]) }}" method="POST">
+							@csrf
+							@method('DELETE')
+							<button class="btn text-sm md:text-base hover:bg-red-lightest text-red-darkest lg:text-lg">
+								<i class="fas fa-dumpster-fire"></i> Delete
+							</button>
+						</form>
+					</div>
+					<form action="{{ route('event.update', $event ) }}" method="POST">
+						@csrf
+						@method('PUT')
 
-								<inv-form-group label="Name" name="name" value="{{ old('name', $event->name) }}"
-								                error-text="{{ $errors->first('name') }}"></inv-form-group>
+						<inv-form-group label="Name" name="name" value="{{ old('name', $event->name) }}"
+						                error-text="{{ $errors->first('name') }}"></inv-form-group>
 
-								<inv-form-group label="Address" name="address"
-								                value="{{ old('address', $event->address) }}"
-								                error-text="{{ $errors->first('address') }}"></inv-form-group>
+						<inv-form-group label="Address" name="address"
+						                value="{{ old('address', $event->address) }}"
+						                error-text="{{ $errors->first('address') }}"></inv-form-group>
 
-								<inv-form-group label="Description" name="description"
-								                type="textarea"
-								                value="{{  old('description', $event->description) }}"
-								                error-text="{{ $errors->first('description') }}">
-								</inv-form-group>
+						<inv-form-group label="Description" name="description"
+						                type="textarea"
+						                value="{{  old('description', $event->description) }}"
+						                error-text="{{ $errors->first('description') }}">
+						</inv-form-group>
 
-								<inv-form-group label="RSVP" name="rsvp_by" type="dateTime"
-								                value="{{ old('rsvp_by', Carbon::parse($event->rsvp_by)->format('c')) }}"
-								                error-text="{{ $errors->first('rsvp-by') }}"
-								></inv-form-group>
+						<inv-form-group label="RSVP" name="rsvp_by" type="dateTime"
+						                value="{{ old('rsvp_by', Carbon::parse($event->rsvp_by)->format('c')) }}"
+						                error-text="{{ $errors->first('rsvp-by') }}"
+						></inv-form-group>
 
-								<inv-form-group label="Starts At" name="starts_at" type="dateTime"
-								                value="{{ old('starts_at', Carbon::parse($event->starts_at)->format('c')) }}"
-								                error-text="{{$errors->first('starts_at')}}"
-								></inv-form-group>
+						<inv-form-group label="Starts At" name="starts_at" type="dateTime"
+						                value="{{ old('starts_at', Carbon::parse($event->starts_at)->format('c')) }}"
+						                error-text="{{$errors->first('starts_at')}}"
+						></inv-form-group>
 
-								<inv-form-group label="Ends At" name="ends_at" type="dateTime"
-								                value="{{ old('ends_at', Carbon::parse($event->ends_at)->format('c')) }}"
-								                error-text="{{$errors->first('ends_at')}}"
-								></inv-form-group>
+						<inv-form-group label="Ends At" name="ends_at" type="dateTime"
+						                value="{{ old('ends_at', Carbon::parse($event->ends_at)->format('c')) }}"
+						                error-text="{{$errors->first('ends_at')}}"
+						></inv-form-group>
 
-								<div class="form-group">
-									<inv-event-repeated :value="{{ $event->repeated }}"
-									                    :current-type="{{ $event->repeated_type_id  }}"
-									                    :types="{{ $repeatedTypes }}"></inv-event-repeated>
-								</div>
+						<div class="form-group">
+							<inv-event-repeated :value="{{ $event->repeated }}"
+							                    :current-type="{{ $event->repeated_type_id  }}"
+							                    :types="{{ $repeatedTypes }}"></inv-event-repeated>
+						</div>
 
-								<div class="flex justify-end">
-									<a href="{{ route('event.index') }}"
-									   class="btn bg-red-dark hover:bg-red text-white font-bold shadow text-sm md:text-base lg:text-lg mr-2">
-										Cancel
-									</a>
+						<div class="flex justify-end">
+							<a href="{{ route('event.index') }}"
+							   class="btn bg-red-dark hover:bg-red text-white font-bold shadow text-sm md:text-base lg:text-lg mr-2">
+								Cancel
+							</a>
 
-									<button class="btn bg-green-dark hover:bg-green text-white font-bold shadow text-sm md:text-base lg:text-lg">
-										Update
-									</button>
-								</div>
-							</form>
+							<button class="btn bg-green-dark hover:bg-green text-white font-bold shadow text-sm md:text-base lg:text-lg">
+								Update
+							</button>
+						</div>
+					</form>
 
 
-							<div class="flex flex-row justify-between mt-10 mb-2 border-grey border-b-2">
-								<div class="self-end text-sm md:text-lg lg:text-2xl font-bold">
-									<i class="fas fa-user-friends"></i>Attendees
-								</div>
+					<div class="flex flex-row justify-between mt-10 mb-2 border-grey border-b-2">
+						<div class="self-end text-sm md:text-lg lg:text-2xl font-bold">
+							<i class="fas fa-user-friends"></i>Attendees
+						</div>
 
-								<div class="pb-1">
-									<inv-attendee-modal :event-id="{{ $event->id }}"
-									                    :is-host="{{auth()->user()->isEventHost($event->id) ? 'true' : 'false'}}"></inv-attendee-modal>
-								</div>
-							</div>
-
-
-							<list-group-scrollable>
-								@foreach($attendees as $attendee)
-									<div class="list-group-item border-0">
-										<div class="flex flex-wrap">
-											<div class="w-3/4">
-												<div class="text-sm font-bold text-grey-darkest md:text-base">
-													{{ $attendee->user->name }}
-												</div>
-												<div class="text-grey-dark text-xs md:text-sm">
-													{{ $attendee->user->email }}
-												</div>
-											</div>
-											<div class="w-1/4 flex justify-end">
-												@if(auth()->user()->isEventHost($event->id) && auth()->user()->id !== $attendee->user_id)
-													<form action="{{ route('attendee.destroy',  $attendee ) }}"
-													      method="POST">
-														@csrf
-														@method('DELETE')
-														<button class="btn bg-red-dark hover:bg-red text-white font-bold text-xs md:text-base lg:text-lg shadow my-1">
-															<i class="fa fa-user-times"></i>
-														</button>
-													</form>
-												@endif
-											</div>
-											<div class="w-full">
-												<inv-attendee-status attendee-type="{{ $attendee->attendeeType->name }}"
-												                     attendee-status="{{ $attendee->attendeeStatus->name }}"></inv-attendee-status>
-											</div>
-										</div>
-										<hr class="p-0 mb-0">
-									</div>
-
-								@endforeach
-							</list-group-scrollable>
+						<div class="pb-1">
+							<inv-attendee-modal :event-id="{{ $event->id }}"
+							                    :is-host="{{auth()->user()->isEventHost($event->id) ? 'true' : 'false'}}"></inv-attendee-modal>
 						</div>
 					</div>
-{{--				</div>--}}
-{{--			</div>--}}
+
+
+					<list-group-scrollable>
+						@foreach($attendees as $attendee)
+							<div class="list-group-item border-0">
+								<div class="flex flex-wrap">
+									<div class="w-3/4">
+										<div class="text-sm font-bold text-grey-darkest md:text-base">
+											{{ $attendee->user->name }}
+										</div>
+										<div class="text-grey-dark text-xs md:text-sm">
+											{{ $attendee->user->email }}
+										</div>
+									</div>
+									<div class="w-1/4 flex justify-end">
+										@if(auth()->user()->isEventHost($event->id) && auth()->user()->id !== $attendee->user_id)
+											<form action="{{ route('attendee.destroy',  $attendee ) }}"
+											      method="POST">
+												@csrf
+												@method('DELETE')
+												<button class="btn bg-red-dark hover:bg-red text-white font-bold text-xs md:text-base lg:text-lg shadow my-1">
+													<i class="fa fa-user-times"></i>
+												</button>
+											</form>
+										@endif
+									</div>
+									<div class="w-full">
+										<inv-attendee-status attendee-type="{{ $attendee->attendeeType->name }}"
+										                     attendee-status="{{ $attendee->attendeeStatus->name }}"></inv-attendee-status>
+									</div>
+								</div>
+								<hr class="p-0 mb-0">
+							</div>
+
+						@endforeach
+					</list-group-scrollable>
+				</div>
+			</div>
 		</div>
 	</div>
 
--- resources/views/event/show.blade.php
+++ resources/views/event/show.blade.php
@@ -4,116 +4,95 @@
 @section('content')
 
 	<div class="container">
-		<div class="col-12">
-			<div class="row">
-				<div class="col">
-					<div class="card">
-						<div class="card-body">
-							<div class="flex flex-wrap justify-between">
-								<div class="w-1/2 self-center text-sm md:text-lg lg:text-2xl ">
-									<i class="fas fa-calendar-plus"></i> {{ $event->name }}
-								</div>
-								<div class="w-1/2 self-center flex justify-end">
+		<div class="flex w-full justify-center">
+			<div class="card shadow w-full sm:w-3/4 lg:w-1/2">
+				<div class="card-body">
+					<div class="flex flex-wrap justify-between">
+						<div class="w-1/2 self-center text-sm md:text-lg lg:text-2xl ">
+							<i class="fas fa-calendar-plus"></i> {{ $event->name }}
+						</div>
+						<div class="w-1/2 self-center flex justify-end">
 
-								@if(!auth()->user()->isAttending($event->id ))
-									<form action="{{ route('attendee.store', ['userInvites' => [auth()->user()->id], 'eventId' => $event->id ]) }}"
-									      method="POST">
-										@method('POST')
-										@csrf
-										<button class="btn bg-green-dark hover:bg-green text-white font-bold shadow text-sm md:text-base lg:text-lg ">
-											<i class="fas fa-calendar-plus mr-2"></i>Attend
-										</button>
-									</form>
-								@else
-									<form action="{{ route('attendee.destroy', auth()->user()->getUserAttendee($event->id) ) }}"
-									      method="POST">
-										@method('DELETE')
-										@csrf
-										<button class="btn text-sm md:text-base hover:bg-red-lightest text-red-darkest lg:text-lg">
-											<i class="fas fa-dumpster-fire mr-2"></i>Leave
-										</button>
-									</form>
-								@endif
-								</div>
-							</div>
-							<hr>
-							<inv-form-group label="Name" name="name" value="{{ $event->name }}"
-							                is-disabled></inv-form-group>
-
-							<inv-form-group label="Address" name="address"
-							                value="{{ $event->address }}" is-disabled></inv-form-group>
-
-							<inv-form-group label="Description" name="description"
-							                type="textarea"
-							                value="{{  $event->description }}" is-disabled>
-							</inv-form-group>
-
-							<inv-form-group label="RSVP" name="rsvp_by"
-							                value="{{ Carbon::parse($event->rsvp_by)->toDayDateTimeString() }}"
-							                is-disabled></inv-form-group>
-
-							<div class="flex flex-wrap">
-									<inv-form-group label="Start Date" name="start_date" class="w-full md:w-1/2 md:pr-2"
-									                type="date"
-									                value="{{  Carbon::parse($event->starts_at)->toDateString() }}"
-									                is-disabled>
-									</inv-form-group>
-									<inv-form-group label="Start Time" name="start_time"  class="w-full md:w-1/2"
-									                type="time"
-									                value="{{ Carbon::parse($event->starts_at)->toTimeString() }}"
-									                is-disabled>
-									</inv-form-group>
-							</div>
-							<div class="flex flex-wrap">
-									<inv-form-group label="End Date" name="end_date" class="w-full md:w-1/2 md:pr-2"
-									                type="date"
-									                value="{{  Carbon::parse($event->ends_at)->toDateString() }}"
-									                is-disabled>
-									</inv-form-group>
-									<inv-form-group label="End Time" name="end_time" class="w-full md:w-1/2"
-									                type="time"
-									                value="{{ Carbon::parse($event->ends_at)->toTimeString() }}"
-									                is-disabled>
-									</inv-form-group>
-								</div>
-
-							<div class="form-group">
-								<inv-event-repeated :value="{{ $event->repeated }}"
-								                    :current-type="{{ $event->repeated_type_id  }}"
-								                    :types="{{ \App\RepeatedType::all() }}" disabled></inv-event-repeated>
-
-							</div>
-
-							<div class="flex flex-row justify-between mt-10">
-								<div class="self-center text-sm md:text-lg lg:text-2xl">
-									<i class="fas fa-user-friends"></i>Attendees
-								</div>
-							</div>
-							<hr>
-							<list-group-scrollable>
-								@foreach($attendees as $attendee)
-									<div class="list-group-item border-0">
-										<div class="flex flex-wrap">
-												<div class="w-full md:w-1/2">
-													<div class="text-sm font-bold text-grey-darkest md:text-base">
-														{{ $attendee->user->name }}
-													</div>
-													<div class="text-grey-dark text-xs md:text-sm">
-														{{ $attendee->user->email }}
-													</div>
-												</div>
-												<div class="w-full md:w-1/2">
-													<inv-attendee-status attendee-type="{{ $attendee->attendeeType->name }}"
-													                     attendee-status="{{ $attendee->attendeeStatus->name }}"></inv-attendee-status>
-												</div>
-											</div>
-										</div>
-										<hr>
-								@endforeach
-							</list-group-scrollable>
-
+							@if(!auth()->user()->isAttending($event->id ))
+								<form action="{{ route('attendee.store', ['userInvites' => [auth()->user()->id], 'eventId' => $event->id ]) }}"
+								      method="POST">
+									@method('POST')
+									@csrf
+									<button class="btn bg-green-dark hover:bg-green text-white font-bold shadow text-sm md:text-base lg:text-lg ">
+										<i class="fas fa-calendar-plus mr-2"></i>Attend
+									</button>
+								</form>
+							@else
+								<form action="{{ route('attendee.destroy', auth()->user()->getUserAttendee($event->id) ) }}"
+								      method="POST">
+									@method('DELETE')
+									@csrf
+									<button class="btn text-sm md:text-base hover:bg-red-lightest text-red-darkest lg:text-lg">
+										<i class="fas fa-dumpster-fire mr-2"></i>Leave
+									</button>
+								</form>
+							@endif
 						</div>
 					</div>
+					<hr>
+					<inv-form-group label="Name" name="name" value="{{ $event->name }}"
+					                is-disabled></inv-form-group>
+
+					<inv-form-group label="Address" name="address"
+					                value="{{ $event->address }}" is-disabled></inv-form-group>
+
+					<inv-form-group label="Description" name="description"
+					                type="textarea"
+					                value="{{  $event->description }}" is-disabled>
+					</inv-form-group>
+
+					<inv-form-group label="RSVP"
+					                value="{{ Carbon::parse($event->rsvp_by)->toDayDateTimeString() }}"
+					                is-disabled></inv-form-group>
+
+					<inv-form-group label="Starts At"
+					                value="{{ Carbon::parse($event->starts_at)->toDayDateTimeString() }}"
+					                is-disabled></inv-form-group>
+
+					<inv-form-group label="Ends At"
+					                value="{{ Carbon::parse($event->ends_at)->toDayDateTimeString() }}"
+					                is-disabled></inv-form-group>
+
+					<div class="form-group">
+						<inv-event-repeated :value="{{ $event->repeated }}"
+						                    :current-type="{{ $event->repeated_type_id  }}"
+						                    :types="{{ $repeatedTypes }}" disabled></inv-event-repeated>
+
+					</div>
+
+					<div class="flex flex-row justify-between mt-10">
+						<div class="self-center text-sm md:text-lg lg:text-2xl">
+							<i class="fas fa-user-friends"></i>Attendees
+						</div>
+					</div>
+					<hr>
+					<list-group-scrollable>
+						@foreach($attendees as $attendee)
+							<div class="list-group-item border-0">
+								<div class="flex flex-wrap">
+									<div class="w-full md:w-1/2">
+										<div class="text-sm font-bold text-grey-darkest md:text-base">
+											{{ $attendee->user->name }}
+										</div>
+										<div class="text-grey-dark text-xs md:text-sm">
+											{{ $attendee->user->email }}
+										</div>
+									</div>
+									<div class="w-full md:w-1/2">
+										<inv-attendee-status attendee-type="{{ $attendee->attendeeType->name }}"
+										                     attendee-status="{{ $attendee->attendeeStatus->name }}"></inv-attendee-status>
+									</div>
+								</div>
+							</div>
+							<hr>
+						@endforeach
+					</list-group-scrollable>
+
 				</div>
 			</div>
 		</div>
